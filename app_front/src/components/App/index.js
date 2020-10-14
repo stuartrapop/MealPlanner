c87cc8667a53c635691cbffd8f346a913e9110af
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-10-14 14:56:42 +0200

    components header/main/footer created

--- app_front/src/components/App/index.js
+++ app_front/src/components/App/index.js
@@ -2,14 +2,18 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import Header from '../Header';
+import Footer from '../Footer';
+import Main from '../Main';
+import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <Header />
+    <Main />
+    <Footer />
+
   </div>
 );
 
