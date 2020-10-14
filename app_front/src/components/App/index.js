6efe51361048fd50e2547420bf2b546cbddafdd5
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-10-14 14:16:30 +0200

    componant counter removed from app

--- app_front/src/components/App/index.js
+++ app_front/src/components/App/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 
 // == Import
-import Counter from 'src/containers/Counter';
 import reactLogo from './react-logo.svg';
 import './styles.css';
 
@@ -11,7 +10,6 @@ const App = () => (
   <div className="app">
     <img src={reactLogo} alt="react logo" />
     <h1>Composant : App</h1>
-    <Counter />
   </div>
 );
 
