02d93c67982a3ef9981c8d43e40e57168e7d05f7
stuartrapop <stuartrapop@gmail.com>
2020-10-15 13:09:37 +0200

    seeding

--- app_back/resources/database_creation_and_seed.sql
+++ app_back/resources/database_creation_and_seed.sql
@@ -6,8 +6,12 @@ DROP TABLE IF EXISTS "meal";
 DROP TABLE IF EXISTS "group";
 DROP TABLE IF EXISTS "user";
 DROP TABLE IF EXISTS "review";
+DROP TABLE IF EXISTS "type";
+DROP TABLE IF EXISTS "family";
 
-
+DROP TABLE IF EXISTS "type_defines_recipe";
+DROP TABLE IF EXISTS "family_describes_ingredient";
+DROP TABLE IF EXISTS "user_likes_recipe";
 DROP TABLE IF EXISTS "user_belongs_group";
 DROP TABLE IF EXISTS "recipe_contains_ingredient";
 DROP TABLE IF EXISTS "meal_has_recipe";
@@ -16,10 +20,8 @@ DROP TABLE IF EXISTS "meal_has_recipe";
 
 CREATE TABLE "ingredient" (
     "id"  SERIAL PRIMARY KEY,
-    "name" VARCHAR NOT NULL,
-    "family" VARCHAR NOT NULL,
-    "group_1" VARCHAR,
-    "group_2" VARCHAR,
+    "name" VARCHAR(30) NOT NULL,
+    "url" VARCHAR(100),
     "weight" BOOLEAN NOT NULL,
     "volume" BOOLEAN NOT NULL,
     "countable" BOOLEAN NOT NULL,
@@ -29,38 +31,38 @@ CREATE TABLE "ingredient" (
 
 CREATE TABLE "recipe" (
     "id"  SERIAL PRIMARY KEY,
-    "title" VARCHAR NOT NULL,
-    "difficulty" VARCHAR,
-    "instructions" VARCHAR,
-    "type" VARCHAR,
-    "cooking_time" VARCHAR,
-    "user_id" integer  REFERENCES "user" ("id"),
+    "title" VARCHAR(30) NOT NULL,
+    "url" VARCHAR(100) NOT NULL,
+    "difficulty" VARCHAR(15),
+    "instructions" TEXT,
+    "cooking_time" INTEGER,
+    "user_id" INTEGER  REFERENCES "user" ("id"),
     "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
     "updatedAt" TIMESTAMP
 
 CREATE TABLE "meal" (
     "id"  SERIAL PRIMARY KEY,
     "day" DATE NOT NULL,
-    "time" VARCHAR NOT NULL,
-    "group_id" integer  REFERENCES "group" ("id"),
+    "time" VARCHAR (25) NOT NULL,
+    "group_id" INTEGER  REFERENCES "group" ("id"),
     "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
     "updatedAt" TIMESTAMP
 );
 
 CREATE TABLE "group" (
     "id"  SERIAL PRIMARY KEY,
-    "name" VARCHAR NOT NULL,
+    "name" VARCHAR(30) NOT NULL,
     "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
     "updatedAt" TIMESTAMP
 );
 
 CREATE TABLE "user" (
     "id"  SERIAL PRIMARY KEY,
-    "firstname" VARCHAR NOT NULL,
-    "lastname" VARCHAR NOT NULL,
-    "username" VARCHAR NOT NULL,
-    "password" VARCHAR NOT NULL,
-    "email" VARCHAR NOT NULL,
+    "first_name" VARCHAR(50) NOT NULL,
+    "last_name" VARCHAR(50) NOT NULL,
+    "user_name" VARCHAR(20) NOT NULL,
+    "password" VARCHAR(70) NOT NULL,
+    "email" VARCHAR(50) NOT NULL,
     "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
     "updatedAt" TIMESTAMP
 );
@@ -68,7 +70,7 @@ CREATE TABLE "user" (
 
 CREATE TABLE "review" (
     "id"  SERIAL PRIMARY KEY,
-    "comment" VARCHAR,
+    "comment" TEXT,
     "grade" INTEGER NOT NULL,
     "recipe_id" INTEGER  REFERENCES "recipe" ("id"),
     "user_id" INTEGER  REFERENCES "user" ("id"),
@@ -76,13 +78,48 @@ CREATE TABLE "review" (
     "updatedAt" TIMESTAMP
 );
 
+CREATE TABLE "type" (
+    "id"  SERIAL PRIMARY KEY,
+    "name" VARCHAR(20) NOT NULL,
+    "grade" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updatedAt" TIMESTAMP
+);
 
+CREATE TABLE "family" (
+    "id"  SERIAL PRIMARY KEY,
+    "name" VARCHAR(20) NOT NULL,
+    "grade" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updatedAt" TIMESTAMP
+);
+
+CREATE TABLE "type_defines_recipe" (
+    "type_id" INTEGER  REFERENCES "type" ("id"),
+    "recipe_id" INTEGER REFERENCES "recipe" ("id"),
+    "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updatedAt" TIMESTAMP
+);
+
+CREATE TABLE "family_describes_ingredient" (
+    "family_id" INTEGER  REFERENCES "family" ("id"),
+    "ingredient_id" INTEGER REFERENCES "ingredient" ("id"),
+    "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updatedAt" TIMESTAMP
+);
+
+CREATE TABLE "user_likes_recipe" (
+    "recipe_id" INTEGER  REFERENCES "recipe" ("id"),
+    "user_id" INTEGER REFERENCES "user" ("id"),
+    "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updatedAt" TIMESTAMP
+);
 
 
 CREATE TABLE "user_belongs_group" (
     "user_id" INTEGER  REFERENCES "user" ("id"),
     "group_id" INTEGER REFERENCES "group" ("id"),
-    "user_role" VARCHAR  NOT NULL,
+    "user_role" VARCHAR (25)  NOT NULL,
     "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
     "updatedAt" TIMESTAMP
 );
@@ -91,6 +128,7 @@ CREATE TABLE "recipe_contains_ingredient" (
     "recipe_id" INTEGER  REFERENCES "recipe" ("id"),
     "ingredient_id" INTEGER  REFERENCES "ingredient" ("id"),
     "quantity" INTEGER  NOT NULL,
+    "unit" VARCHAR (20)  NOT NULL,
     "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
     "updatedAt" TIMESTAMP
 );
@@ -113,13 +151,13 @@ BEGIN;
 --
 
 
-INSERT INTO  "ingredient" ("id", "name" , "family" , "weight" , "volume","countable" ) VALUES 
-(1, 'poulet', 'viande', true, false, false ),
-(2, 'orange', 'fruit', false, false, true ),
-(3, 'cerise', 'fruit', true, false, false ),
-(4, 'laitue', 'legume', true, false, false ),
-(5, 'porc', 'viande', true, false, false ),
-(6, 'lait', 'laitage', false, true, false )
+INSERT INTO  "ingredient" ("id", "name" , "url", "weight" , "volume","countable" ) VALUES 
+(1, 'poulet', "https://upload.wikimedia.org/wikipedia/commons/5/57/Chickens_in_market.jpg", true, false, false ),
+(2, 'orange', "https://upload.wikimedia.org/wikipedia/commons/4/43/Ambersweet_oranges.jpg", false, false, true ),
+(3, 'cerise', "https://upload.wikimedia.org/wikipedia/commons/0/01/%22Help_yourself%22_-_Flickr_-_Axel-D.jpg", true, false, false ),
+(4, 'laitue', "https://upload.wikimedia.org/wikipedia/commons/6/6b/Romaine_lettuce.jpg", true, false, false ),
+(5, 'porc', "https://upload.wikimedia.org/wikipedia/commons/b/bd/Cotelettes_de_porc.jpg", true, false, false ),
+(6, 'lait', "https://upload.wikimedia.org/wikipedia/commons/e/e1/Milk_carton_clip_art.png", false, true, false )
 
 
 INSERT INTO "list" ("id", "name", "position") VALUES
