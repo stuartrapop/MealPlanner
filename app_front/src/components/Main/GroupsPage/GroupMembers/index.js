fac6d29c20e04aff5d8d4e15f056dcdf6fd10517
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-10-15 14:15:26 +0200

    GroupsPage - Style mobile & Desktop OK

--- app_front/src/components/Main/GroupsPage/GroupMembers/index.js
+++ app_front/src/components/Main/GroupsPage/GroupMembers/index.js
@@ -18,7 +18,12 @@ const GroupMembers = ({ targetGroup }) => (
         );
       })}
     </ul>
-    <a className="add_member">Ajouter un membre</a>
+    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="25" cy="25" r="23.5" fill="white" stroke="#2ECC71" strokeWidth="3" />
+      <line x1="25.5" y1="17" x2="25.5" y2="34" stroke="#2ECC71" strokeWidth="3" />
+      <line x1="34" y1="25.5" x2="17" y2="25.5" stroke="#2ECC71" strokeWidth="3" />
+    </svg>
+
   </div>
 );
 
