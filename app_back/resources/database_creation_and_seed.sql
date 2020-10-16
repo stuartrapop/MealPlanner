37a69cf018e5a600d5f142f6e2a2d52f089fd851
stuartrapop <stuartrapop@gmail.com>
2020-10-16 15:35:23 +0200

    create first relations in sequelize

--- app_back/resources/database_creation_and_seed.sql
+++ app_back/resources/database_creation_and_seed.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS "ingredient", "recipe", "meal", "group", "user", "review", 
 
 
 CREATE TABLE "ingredient" (
-    "id"  SERIAL PRIMARY KEY,
+    "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "name" VARCHAR(30) NOT NULL,
     "url" VARCHAR(100),
     "weight" BOOLEAN NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE "ingredient" (
 );
 
 CREATE TABLE "user" (
-    "id"  SERIAL PRIMARY KEY,
+    "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "first_name" VARCHAR(50) NOT NULL,
     "last_name" VARCHAR(50) NOT NULL,
     "user_name" VARCHAR(20) NOT NULL,
@@ -28,7 +28,7 @@ CREATE TABLE "user" (
 
 
 CREATE TABLE "recipe" (
-    "id"  SERIAL PRIMARY KEY,
+    "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "title" VARCHAR(100) NOT NULL,
     "url" VARCHAR  NOT NULL,
     "difficulty" VARCHAR(15),
@@ -41,14 +41,14 @@ CREATE TABLE "recipe" (
 );
 
 CREATE TABLE "group" (
-    "id"  SERIAL PRIMARY KEY,
+    "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "name" VARCHAR(30) NOT NULL,
     "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
     "updatedAt" TIMESTAMP
 );
 
 CREATE TABLE "meal" (
-    "id"  SERIAL PRIMARY KEY,
+    "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "day" DATE NOT NULL,
     "time" VARCHAR (25) NOT NULL,
     "group_id" INTEGER  REFERENCES "group" ("id"),
@@ -61,7 +61,7 @@ CREATE TABLE "meal" (
 
 
 CREATE TABLE "review" (
-    "id"  SERIAL PRIMARY KEY,
+    "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "comment" TEXT,
     "grade" INTEGER NOT NULL,
     "recipe_id" INTEGER  REFERENCES "recipe" ("id"),
@@ -71,14 +71,14 @@ CREATE TABLE "review" (
 );
 
 CREATE TABLE "type" (
-    "id"  SERIAL PRIMARY KEY,
+    "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "name" VARCHAR(20) NOT NULL,
     "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
     "updatedAt" TIMESTAMP
 );
 
 CREATE TABLE "family" (
-    "id"  SERIAL PRIMARY KEY,
+    "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "name" VARCHAR(20) NOT NULL,
     "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
     "updatedAt" TIMESTAMP
@@ -142,7 +142,8 @@ BEGIN;
 --
 
 
-INSERT INTO  "ingredient" ("id", "name" , "weight" , "volume","countable" ) VALUES 
+INSERT INTO  "ingredient" ("id", "name" , "weight" , "volume","countable" ) OVERRIDING SYSTEM VALUE
+VALUES 
 (1, 'poulet', true, false, false ),
 (2, 'orange', false, false, true ),
 (3, 'cerise',  true, false, false ),
@@ -161,7 +162,8 @@ INSERT INTO  "ingredient" ("id", "name" , "weight" , "volume","countable" ) VALU
 (16, 'carrottes',  true, false, false );
 
 
-INSERT INTO  "user" ("id", "first_name", "last_name", "user_name", "password", "email" )  VALUES 
+INSERT INTO  "user" ("id", "first_name", "last_name", "user_name", "password", "email" )  OVERRIDING SYSTEM VALUE
+VALUES 
 (1, 'Pierre', 'Dupont', 'pierre1','French', 'pierre@gmail.com'),
 (2, 'Alice', 'Dupont', 'alice1','French1', 'alice@gmail.com'),
 (3, 'Carole', 'Dupont', 'carole1','French2', 'carole@gmail.com'),
@@ -171,6 +173,7 @@ INSERT INTO  "user" ("id", "first_name", "last_name", "user_name", "password", "
 
 
 INSERT INTO  "recipe" ("id", "title" , "url","difficulty", "instructions" , "cooking_time","number_people", "user_id" ) 
+OVERRIDING SYSTEM VALUE
 VALUES 
 (1, 'Pizza', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Pitigliano%2C_Italy_%285731636322%29.jpg/800px-Pitigliano%2C_Italy_%285731636322%29.jpg', 'Moyen', 'Dans un bol, faites un puis avec la farine, et mettez le sel d un côté, la levure de l autre - faites en sorte de ne pas mettre en contact le sel et la levure au démarrage - Ajoutez l eau au centre, mélangez pour obtenir une pâte homogène, et pétrissez 10 minutes à la main sur le plan de travail ou à l aide d une machine à pain ou d un robot avec le crochet pétrisseur Ajoutez l huile d olive et pétrissez encore 5 minutes Couvrez d un film alimentaire au contact de la pâte, et laissez lever 1 heure à température ambiante, la pâte doit doubler de volume Chassez l air de la pâte, en appuyant dessus avec la main, puis divisez la pâte en 2 ou 3 boules selon le nombre de pizzas souhaitées Laissez les boules de pâte reposer 30 minutes en les couvrant de film alimentaire', '60' ,2, 1),
 (2, 'Spaghetti Bolognaise', 'https://upload.wikimedia.org/wikipedia/commons/d/da/2015-05-24_Bolognesesauce_anagoria.JPG', 'Facile', 'Hachez l''ail, l''oignon, puis coupez la carotte et le céleri en petits dés (enlevez les principales nervures du céleri). Faites chauffer l''huile dans une casserole assez grande. Faites revenir l''ail, l''oignon, la carotte et le céleri à feu doux pendant 5 min en remuant. Augmenter la flamme, puis ajoutez le boeuf. Faites brunir et remuez de façon à ce que la viande ne fasse pas de gros paquets. Ajoutez le bouillon, le vin rouge, les tomates préalablement coupées assez grossièrement, le sucre et le persil haché. Portez à ébullition. Baisser ensuite le feu et laissez mijoter à couvert 1h à 1h30, de façon à ce que le vin s''évapore. Faites cuire les spaghetti, puis mettez-les dans un plat. Ajoutez la sauce bolognaise', '90' , 4, 1),
@@ -180,7 +183,8 @@ Mettre à cuire environ 10 à 15 min dans une cocotte minute.
 Une fois cuits, mettre les légumes dans le bol du mixeur avec la crème fraiche et le beurre. Assaisonner selon votre convenance. Mixer le tout jusqu''à l''obtention d''une purée lisse. Servir chaud.', '60' ,2, 2);
 
 
-INSERT INTO  "group" ("id", "name" ) VALUES 
+INSERT INTO  "group" ("id", "name" ) OVERRIDING SYSTEM VALUE
+VALUES 
 (1, 'Famille Dupont'),
 (2, 'Vacances Ski Oclock' ),
 (3, 'Bureau de developpeurs'),
@@ -188,7 +192,8 @@ INSERT INTO  "group" ("id", "name" ) VALUES
 (5, 'Semaine Karting'),
 (6, 'Fête Dongeons et Dragons');
 
-INSERT INTO  "meal" ("id", "day" , "time", "group_id" ) VALUES 
+INSERT INTO  "meal" ("id", "day" , "time", "group_id" ) OVERRIDING SYSTEM VALUE
+VALUES 
 (1, '18/10/15', 'lunch', 1 ),
 (2, '18/10/15', 'dinner', 1 ),
 (3, '20/10/15', 'lunch', 1 ),
@@ -201,7 +206,8 @@ INSERT INTO  "meal" ("id", "day" , "time", "group_id" ) VALUES
 
 
 
-INSERT INTO  "review" ("id", "comment", "grade", "recipe_id", "user_id")  VALUES
+INSERT INTO  "review" ("id", "comment", "grade", "recipe_id", "user_id")  OVERRIDING SYSTEM VALUE
+VALUES 
 (1, 'L''auteur devrait revoir sa copie et peut-être investir dans...un Bescherelle avant d''écrire des recettes.', 1, 1,1),
 (2, 'Trop Bon. Miam', 10, 1,3),
 (3, 'Trop piquant. Ne mets pas du poivre. Mais sinon, je le recommande fortement', 8, 1,5),
@@ -209,7 +215,8 @@ INSERT INTO  "review" ("id", "comment", "grade", "recipe_id", "user_id")  VALUES
 (5, 'Je referai.', 8, 2,5),
 (6, null, 1, 2,5);
 
-INSERT INTO  "type" ("id", "name")  VALUES
+INSERT INTO  "type" ("id", "name")  OVERRIDING SYSTEM VALUE
+VALUES 
 (1, 'viande'),
 (2, 'végétarien'),
 (3, 'végan'),
@@ -221,7 +228,8 @@ INSERT INTO  "type" ("id", "name")  VALUES
 
 
 
-INSERT INTO  "family" ("id", "name")  VALUES
+INSERT INTO  "family" ("id", "name")  OVERRIDING SYSTEM VALUE
+VALUES 
 (1, 'viande'),
 (2, 'legume'),
 (3, 'fruit'),
@@ -242,15 +250,15 @@ INSERT INTO  "recipe_contains_ingredient" ("recipe_id", "ingredient_id", "quanti
 (1, 7, 0.3, null),
 (1, 8, 0.5, null),
 (1, 9, 0.2, null),
-(2, 9, 0.2 null),
-(2, 10, 0.5 null),
-(2, 11, 0.5 null),
-(3, 11, 0.5 null),
-(3, 12, 3 null),
-(3, 13, 6 null),
-(4, 14, 0.2 null),
-(4, 15, 2 null),
-(4, 16, 2 null),
+(2, 9, 0.2, null),
+(2, 10, 0.5, null),
+(2, 11, 0.5, null),
+(3, 11, 0.5, null),
+(3, 12, 3, null),
+(3, 13, 6, null),
+(4, 14, 0.2, null),
+(4, 15, 2, null),
+(4, 16, 2, null);
 
 
 INSERT INTO  "family_describes_ingredient" ("family_id", "ingredient_id")  VALUES
