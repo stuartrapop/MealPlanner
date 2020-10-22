59698a6c48be29d04d4825a644d340677f57a34e
Clément H <etudiant@teleporteur.oclock>
2020-10-22 14:33:34 +0200

    Front fonctionnel

--- app_front/src/components/Main/Landing/MySpace/index.js
+++ app_front/src/components/Main/Landing/MySpace/index.js
@@ -4,17 +4,12 @@ import Planning from './Planning';
 
 import './styles.scss';
 
-const MySpace = ({ fetchAllGroupsData }) => {
-  useEffect(() => {
-    fetchAllGroupsData();
-  }, []);
-  return (
-    <div className="myspace__container">
-      <AddMeal />
-      <Planning />
-      <a>Génerer ma liste de courses</a>
-    </div>
-  );
-};
+const MySpace = () => (
+  <div className="myspace__container">
+    <AddMeal />
+    <Planning />
+    <a>Génerer ma liste de courses</a>
+  </div>
+);
 
 export default MySpace;
