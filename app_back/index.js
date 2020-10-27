e3eb373951e819741d5ae992cc3ede4035f033e6
stuartrapop <stuartrapop@gmail.com>
2020-10-27 11:15:08 +0100

    experiment

--- app_back/index.js
+++ app_back/index.js
@@ -4,9 +4,35 @@ const session = require('express-session');
 
 const bodyParser = require('body-parser');
 
+// experiment with http-proxy-middleware
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const options = {
+  target: 'http://amanger.com', // target host
+  changeOrigin: true, // needed for virtual hosted sites
+  ws: true, // proxy websockets
+  pathRewrite: {
+    '^/api/old-path': '/api/new-path', // rewrite path
+    '^/api/remove/path': '/path', // remove base path
+  },
+
+  router: {
+    // when request.headers.host == 'dev.localhost:3000',
+    // override target 'http://www.example.org' to 'http://localhost:8000'
+    'dev.localhost:3000': 'http://localhost:8000',
+  },
+};
+
+// create the proxy (without context)
+const exampleProxy = createProxyMiddleware(options);
+
 const app = express();
 const sanitizeHtml = require('sanitize-html');
 const cors = require('cors');
+
+// app.use('/api', exampleProxy);
+
+app.use('/api', createProxyMiddleware({ target: 'http://amanger.com', changeOrigin: true }));
 const router = require('./app/router');
 
 app.use(bodyParser.json());
