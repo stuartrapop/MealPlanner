922aa24e4accbcfdd582a338fdf1b8ca3898e422
stuartrapop <stuartrapop@gmail.com>
2020-11-01 15:57:28 +0100

    added unsplash images

--- app_front/src/components/Footer/index.js
+++ app_front/src/components/Footer/index.js
@@ -10,23 +10,10 @@ import './styles.scss';
 // == Composant
 const Footer = () => (
   <div className="footer">
-    {/*  <div className="separator" />
-    <div className="footer__content">
-      <div className="top__part">
-        <a href="/about"><p> À propos de nous</p></a>
-        <p>Confidentialité</p>
-      </div>
-      <div className="bottom__part">
-
-        <p><Icon copyright name="copyright" /> aManger 2020 - Made in Jason</p>
-      </div>
-    </div> */}
 
     <Divider inverted section />
 
-
-
-    <List id="footer__list" horizontal equal width grid link>
+    <List id="footer__list" equal width grid link>
       <List.Item as="a"><Link to="/about"> À propos de nous</Link></List.Item>
       <List.Item as="a">Confidentialité</List.Item>
       <List.Item as="a"><Icon copyright name="copyright" /> aManger 2020 - Made in Jason</List.Item>
